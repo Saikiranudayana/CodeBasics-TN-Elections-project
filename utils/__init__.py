551d5181f0a17e0429c9b444d8d1e7f1f468d2b6
Saikiranudayana <saiki@atliq.com>
2026-05-16 08:17:37 +0530

    fix: compat patch for Styler.map on pandas <2.1

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,9 @@
 # utils package
+
+# Pandas compat: Styler.map was added in 2.1; alias applymap for older versions
+try:
+    from pandas.io.formats.style import Styler as _Styler
+    if not hasattr(_Styler, "map"):
+        _Styler.map = _Styler.applymap
+except Exception:
+    pass
